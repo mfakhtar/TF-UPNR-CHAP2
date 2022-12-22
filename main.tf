ff2baefe133b29a9c5f5938e5ebcc438d3321670
terraform-edu <terraform-edu@users.noreply.github.com>
2022-12-22 12:22:16 +0530

    add userdata and security group

--- main.tf
+++ main.tf
@@ -6,7 +6,26 @@ resource "aws_instance" "example" {
     ami = "ami-07ffb2f4d65357b42"
     instance_type = "t2.micro"
 
+    user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello World from Fawaz" > index.html
+    noup busybox httpd -f -p 8080 &
+    EOF
+
+  user_data_replace_on_change = true
+
     tags = {
       Name = "fawaz-terraform-eg"
+    }
+}
+
+resource "aws_security_group" "tf-upnr-fawaz-sg" {
+    name = "tf-upnr-fawaz-sg"
+
+    ingress {
+      from_port = "8080"
+      to_port = "8080"
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
     }
 }
