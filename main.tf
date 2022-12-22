0e2af83d9e87dec0f01a459f9294918eef9d30b3
terraform-edu <terraform-edu@users.noreply.github.com>
2022-12-22 14:31:50 +0530

    ASG addition

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
+data "aws_vpc" "mum-default-vpc" {
+  default = true
+}
+
+data "aws_subnets" "mum-default-subnets" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.mum-default-vpc.id]
+  }
+}
+
 provider "aws" {
     region = "ap-south-1"      
 }
 
+/*
 resource "aws_instance" "example" {
     ami = "ami-07ffb2f4d65357b42"
     instance_type = "t2.micro"
@@ -20,7 +32,7 @@ resource "aws_instance" "example" {
       Name = "fawaz-terraform-eg"
     }
 }
-
+*/
 resource "aws_security_group" "tf-upnr-fawaz-sg" {
     name = "tf-upnr-fawaz-sg"
 
@@ -39,6 +51,39 @@ resource "aws_security_group" "tf-upnr-fawaz-sg" {
     }
 }
 
+resource "aws_launch_configuration" "fawaz-webserver-lc" {
+  image_id = "ami-07ffb2f4d65357b42"
+  instance_type = "t2.micro"
+  security_groups = [ aws_security_group.tf-upnr-fawaz-sg.id ]
+  user_data = <<-EOF
+    #!/usr/bin/bash
+    echo "Hello World from Fawaz" > index.html
+    nohup busybox httpd -f -p ${var.webserver-port} &
+  EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }  
+  
+}
+
+resource "aws_autoscaling_group" "fawaz-asg" {
+  launch_configuration = aws_launch_configuration.fawaz-webserver-lc.name
+  vpc_zone_identifier = data.aws_subnets.mum-default-subnets.ids
+
+  min_size = 1
+  max_size = 2
+
+  tag {
+    key = "Name"
+    value = "fawaz-terraform-eg-asg"
+    propagate_at_launch = true
+  }
+  
+}
+
+/*
 output "tf-upnr-webserver-ip" {
   value = aws_instance.example.public_dns
 }
+*/
